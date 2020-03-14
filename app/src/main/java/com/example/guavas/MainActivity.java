b221dd41d064b9b413c92cd016001d48a5d05045
jaiqi19 <42953683+jaiqi19@users.noreply.github.com>
2020-03-14 10:52:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/guavas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.guavas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ gradlew.bat
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <item name="android.net.wifi.WifiEnterpriseConfig java.security.cert.X509Certificate getCaCertificate()">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.net.wifi.WifiEnterpriseConfig java.security.cert.X509Certificate[] getCaCertificates()">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.net.wifi.WifiEnterpriseConfig java.security.cert.X509Certificate[] getClientCertificateChain()">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.net.wifi.WifiEnterpriseConfig void setCaCertificate(java.security.cert.X509Certificate) 0">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.net.wifi.WifiEnterpriseConfig void setCaCertificates(java.security.cert.X509Certificate[]) 0">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.net.wifi.WifiManager">
+    <annotation name="android.support.annotation.SystemService">
+      <val name="value" val="android.content.Context.WIFI_SERVICE" />
+    </annotation>
+  </item>
+  <item name="android.net.wifi.WifiManager android.net.DhcpInfo getDhcpInfo()">
+    <annotation name="android.support.annotation.RequiresPermission">
+      <val name="value" val="&quot;android.permission.ACCESS_WIFI_STATE&quot;" />
+    </annotation>
+  </item>
+  <item name="android.net.wifi.WifiManager android.net.wifi.WifiInfo getConnectionInfo()">
+    <annotation name="android.support.annotation.RequiresPermission">
+      <val name="value" val="&quot;android.permission.ACCESS_WIFI_STATE&quot;" />
+    </annotation>
+  </item>
+  <item name="android.net.wifi.WifiManager boolean disableNetwork(int)">
+    <annotation name="android.support.annotation.RequiresPermission">
+      <val name="value" val="&quot;android.permission.CHANGE_WIFI_STATE&quot;" />
+    </annotation>
+  </item>
+  <item name="android.net.wifi.WifiManager boolean disconnect()">
+    <annotation name="android.support.annotation.RequiresPermission">
+      <val name="value" val="&quot;android.permission.CHANGE_WIFI_STATE&quot;" />
+    </annotation>
+  </item>
+  <item name="android.net.wifi.WifiManager bool
