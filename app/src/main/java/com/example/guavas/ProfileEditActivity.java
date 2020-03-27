aa7f1b353ca6ff6701a1147ed7cc30fb98531209
zaneho <zane.ho@hotmail.com>
2020-03-27 10:28:19 +0800

    Profile form validation added

--- app/src/main/java/com/example/guavas/ProfileEditActivity.java
+++ app/src/main/java/com/example/guavas/ProfileEditActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
+
+import java.util.Objects;
 
 /**
  * This Activity controls Profile update feature.
@@ -89,19 +92,21 @@ public class ProfileEditActivity extends AppCompatActivity {
         displayProfile();
 
         //EVENT LISTENERS
-        radioBtnM.setOnClickListener(new View.OnClickListener(){
+        radioBtnM.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){ onRadioButtonClicked(view);
+            public void onClick(View view) {
+                onRadioButtonClicked(view);
             }
         });
 
-        radioBtnF.setOnClickListener(new View.OnClickListener(){
+        radioBtnF.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){ onRadioButtonClicked(view);
+            public void onClick(View view) {
+                onRadioButtonClicked(view);
             }
         });
 
-        btnSave.setOnClickListener(new View.OnClickListener(){
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateProfile();
@@ -124,7 +129,7 @@ public class ProfileEditActivity extends AppCompatActivity {
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radioBtnGenderM:
                 if (checked)
                     // Gender = Male
@@ -138,16 +143,16 @@ public class ProfileEditActivity extends AppCompatActivity {
         }
     }
 
-    public void displayProfile(){
+    public void displayProfile() {
         DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("UserProfile").child(phone);
         reff.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NotNull DataSnapshot dataSnapshot){//retrieve from db and display
+            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {//retrieve from db and display
                 /*if(dataSnapshot.exists()){
                     maxid=(dataSnapshot.getChildrenCount());
                 }*/
 
-                if(dataSnapshot.exists()) {
+                if (dataSnapshot.exists()) {
                     String fname = dataSnapshot.child("firstName").getValue().toString();
                     String lname = dataSnapshot.child("lastName").getValue().toString();
                     gender = dataSnapshot.child("gender").getValue().toString();
@@ -162,9 +167,9 @@ public class ProfileEditActivity extends AppCompatActivity {
                     txtLastName.setText(lname);
 
 
-                    if(gender.equals("Male")){
+                    if (gender.equals("Male")) {
                         radioGrpGender.check(R.id.radioBtnGenderM);
-                    }else if(gender.equals("Female")){
+                    } else if (gender.equals("Female")) {
                         radioGrpGender.check(R.id.radioBtnGenderF);
                     }
 
@@ -176,30 +181,67 @@ public class ProfileEditActivity extends AppCompatActivity {
                     txtEmail.setText(email);
                 }
             }
-            public void onCancelled(@NotNull DatabaseError databaseError){
+
+            public void onCancelled(@NotNull DatabaseError databaseError) {
 
             }
         });
     }
 
-    public void updateProfile(){
-        updateUserProfile.setFirstName(txtFirstName.getText().toString().trim());
-        updateUserProfile.setLastName(txtLastName.getText().toString().trim());
-        updateUserProfile.setGender(gender);
-        updateUserProfile.setDobD(txtDay.getText().toString().trim());
-        updateUserProfile.setDobM(txtMonth.getText().toString().trim());
-        updateUserProfile.setDobY(txtYear.getText().toString().trim());
+    public void updateProfile() {
+        boolean validate = validateForm();
 
-        int height = Integer.parseInt(txtHeight.getText().toString().trim());
-        updateUserProfile.setHeight(height);
-        int weight = Integer.parseInt(txtWeight.getText().toString().trim());
-        updateUserProfile.setWeight(weight);
+        if (validate) {
+            updateUserProfile.setFirstName(txtFirstName.getText().toString().trim());
+            updateUserProfile.setLastName(txtLastName.getText().toString().trim());
+            updateUserProfile.setGender(gender);
+            updateUserProfile.setDobD(txtDay.getText().toString().trim());
+            updateUserProfile.setDobM(txtMonth.getText().toString().trim());
+            updateUserProfile.setDobY(txtYear.getText().toString().trim());
 
-        updateUserProfile.setEmail(txtEmail.getText().toString().trim());
+            int height = Integer.parseInt(txtHeight.getText().toString().trim());
+            updateUserProfile.setHeight(height);
+            int weight = Integer.parseInt(txtWeight.getText().toString().trim());
+            updateUserProfile.setWeight(weight);
 
-        DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("UserProfile");
-        reff.child(phone).setValue(updateUserProfile);
-        Toast.makeText(ProfileEditActivity.this, "Profile updated success", Toast.LENGTH_LONG).show();
+            updateUserProfile.setEmail(txtEmail.getText().toString().trim());
+
+            DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("UserProfile");
+            reff.child(phone).setValue(updateUserProfile);
+            Toast.makeText(ProfileEditActivity.this, "Profile updated success", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(ProfileEditActivity.this, "Please fill in required fields", Toast.LENGTH_LONG).show();
+        }
+
     }
 
+    private boolean validateForm() {
+        if (txtFirstName.getText().toString().length() == 0) {
+            txtFirstName.setError("First Name cannot be empty");
+            return false;
+        } else if (txtLastName.getText().toString().length() == 0) {
+            txtLastName.setError("Last Name cannot be empty");
+            return false;
+        } else if (txtDay.getText().toString().length() == 0) {
+            txtDay.setError("DOB Day cannot be empty");
+            return false;
+        } else if (txtMonth.getText().toString().length() == 0) {
+            txtMonth.setError("DOB Month cannot be empty");
+            return false;
+        } else if (txtYear.getText().toString().length() == 0) {
+            txtYear.setError("DOB Year cannot be empty");
+            return false;
+        } else if (txtHeight.getText().toString().length() == 0) {
+            txtHeight.setError("Height cannot be empty");
+            return false;
+        } else if (txtWeight.getText().toString().length() == 0) {
+            txtWeight.setError("Weight cannot be empty");
+            return false;
+        } else if (txtEmail.getText().toString().length() == 0) {
+            txtEmail.setError("Email cannot be empty");
+            return false;
+        }
+
+        return true;
+    }
 }
